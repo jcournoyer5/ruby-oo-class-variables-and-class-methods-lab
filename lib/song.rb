1573f2039d9d2a6c557c7d4cda9ca7c05806a66b
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-12 05:32:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,30 @@
 class Song
     
     
-    attr_accessor :name, :artist, :genre
+    attr_accessor :name, :artist, :genre, :count
+    
+    @@count = 0 
 
   def initialize(name, artist, genre)
       @name = name
       @artist = artist
       @genre = genre
+      @@count +=
+  end    
   
+  def class_variable_get=(count)
+    @release_date = date
+  end
 
-      # @@count = 0 
+  def release_date
+    @release_date
+  end
+ 
+      
+      @@count += 1 
       # @@artists = 
       # @@genres =
-  end    
+     
      
 end
  
