5fec22dbcffebcb51fef9f8ea841ce63749732a2
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-12 05:16:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,25 +3,16 @@ class Song
     
     attr_accessor :name, :artist, :genre
 
-  def initialization(name, artist, genre)
+  def initialize(name, artist, genre)
       @name = name
       @artist = artist
       @genre = genre
-  end   
   
-  song = Song.new
-song.name = "99 Problems"
 
-  
-      @@count = 0 
-
-  def initialize
-    @@count += 1
-  end
-
-  def self.count
-    @@count
-  end
-
-
-end    
+      # @@count = 0 
+      # @@artists = 
+      # @@genres =
+  end    
+     
+end
+ 
