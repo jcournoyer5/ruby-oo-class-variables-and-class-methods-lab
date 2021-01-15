8c459f239f9569fd9abc86b62f6ee51042e58ed1
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-15 00:26:02 +0000

    commit tree beccaad12da33187041b1df59d559957a4d86b4c

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
     
     
@@ -33,13 +35,41 @@ class Song
   end  
   
   def self.genre_count
+    genre_count_hash = {}
     
-  end  
+    #   count += 1
+    # else  
+  @@genres.each do |genre|
+    if genre_count_hash[genre]
+      genre_count_hash[genre] += 1
+    else 
+     genre_count_hash[genre] = 1
+     
+    end
+    end
+    genre_count_hash
+  end
   
   def self.artist_count
-  end  
+   artist_count_hash = {}
+  
+  @@artists.each do |artist|
+    if artist_count_hash[artist]
+      artist_count_hash[artist] += 1
+    else
+      artist_count_hash[artist] = 1
+    end
+    end
+    artist_count_hash
+    
+  end    
 
 end     
      
-
+#1.) Checking if the hash already has the key of genre
+#2.) if it doesn't then we need to add it and set the count to 1
+#3.) if it DOES have that key, then we need to increase the count by one
  
+
+      
+  #     # {genres, Song.count} got: {["rap", "pop"]=>3}
